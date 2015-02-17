d98bd7591f9365e62103ddf884af8c10a7912503
Andrea Bisognin <andrea.bisognin@gmail.com>
2015-02-17 23:10:59 +0100

    added vote form and relative view

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render
-from polls.models import Question
+from polls.models import Question, Choice
+from django.core.urlresolvers import reverse
 
-#def index(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    #template = loader.get_template('polls/index.html')
-    #context = RequestContext(request, {'latest_question_list':latest_question_list})
-    #return HttpResponse(template.render(context))
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -22,10 +18,17 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',{'question':question}) 
 
 def results(request,question_id):
-    response =  HttpResponse("You're looking at the results of question %s." % question_id)
-    return response 
-
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question}) 
 
 def vote(request,question_id):
-    response =  HttpResponse("You're voting on question %s." % question_id)
-    return response 
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html', {'question':p,'error_message':'You did not select a choice'})
+    
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
